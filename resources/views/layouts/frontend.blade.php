6030b38482eafadf3f0ebb53cdbfb6c152330969
safroniaziz <safroni.aziz@gmail.com>
2020-10-22 16:19:01 +0700

    new slider

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -123,7 +123,7 @@
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
-            @foreach ($sliders as $index=>$slider)
+            @foreach ($sliders as $slider)
 				<div class="item @if($index == '1') {{ 'active' }} @endif item-100vh">
 					<img src="{{ asset('storage/'.$slider->gambar) }}" alt="banner-slider-1">
 					<div class="carousel-caption banner-slider-inner">
