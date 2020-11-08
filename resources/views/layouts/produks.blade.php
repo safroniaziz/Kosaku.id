74eab61c076f8202c99bebcb7e9894f022f9e8bb
safroniaziz <safroni.aziz@gmail.com>
2020-11-08 11:23:33 +0700

    new commit

--- resources/views/layouts/produks.blade.php
+++ resources/views/layouts/produks.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-    <title>The Nest - Real Estate HTML Template</title>
+	<title>Kosaku.id - Platform Pencarian Kostan </title>
 	<link rel="icon" href="{{ asset('assets/images/logo.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
