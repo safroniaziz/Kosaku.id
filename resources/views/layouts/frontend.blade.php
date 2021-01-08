57622eda2c898a9d8facb4b4b0efa3dbda0ea48a
safroniaziz <safroni.aziz@gmail.com>
2021-01-08 18:29:32 +0700

    new commit

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -61,7 +61,10 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                
+                <div class="list-inline">
+                    <a href="tel:1-8X0-666-8X88"><i class="fa fa-phone"></i>+6285709586249</a>
+                    <a href="tel:info@themevessel.com"><i class="fa fa-envelope"></i>kosakujaya@gmail.com</a>
+                </div>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <ul class="top-social-media pull-right">
