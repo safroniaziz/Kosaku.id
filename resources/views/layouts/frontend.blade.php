59ca3c2c82d0f3afe5380e186a7579b52d773f26
safroniaziz <safroni.aziz@gmail.com>
2021-01-09 08:09:33 +0700

    new commit

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -60,7 +60,7 @@
 <header class="top-header" id="top">
     <div class="container">
         <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
                 <div class="list-inline">
                     @if (Auth::check())
                     @else
@@ -70,7 +70,7 @@
                     <a href=""><i class="fa fa-envelope"></i>kosakujaya@gmail.com</a>
                 </div>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                 <ul class="top-social-media pull-right">
                     <li>
                         @if (Auth::check())
